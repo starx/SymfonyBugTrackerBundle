688cc61b88cd79a848cb0590e348e265d59517fd
starx <mail@mrnepal.com>
2016-04-29 13:54:13 +0100

    Fix Automatic loading of configuration

--- StarxSymfonyBugTrackerBundle.php
+++ StarxSymfonyBugTrackerBundle.php
@@ -2,8 +2,17 @@
 
 namespace Starx\SymfonyBugTrackerBundle;
 
+use Starx\SymfonyBugTrackerBundle\DependencyInjection\StarxSymfonyBugTrackerExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class StarxSymfonyBugTrackerBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        if (null === $this->extension) {
+            $this->extension = new StarxSymfonyBugTrackerExtension();
+        }
+
+        return $this->extension;
+    }
 }
