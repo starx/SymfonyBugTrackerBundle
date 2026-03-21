5953af363669eeb13a0d751dc6c97869336119ad
root <>
2026-03-21 04:42:01 +0000

    Add native bundle and extension return types

--- StarxSymfonyBugTrackerBundle.php
+++ StarxSymfonyBugTrackerBundle.php
@@ -3,11 +3,12 @@
 namespace Starx\SymfonyBugTrackerBundle;
 
 use Starx\SymfonyBugTrackerBundle\DependencyInjection\StarxSymfonyBugTrackerExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class StarxSymfonyBugTrackerBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new StarxSymfonyBugTrackerExtension();
